4240ff0ba71e2e52c6659868293a9ae594e097ac
Jordi <jordi.armadans@hypehunter.io>
2021-06-19 18:13:25 +0200

    Hypehunter webapp, primer commit

---
+++ objects/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Club(models.Model):
+    creator = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    updated_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def update(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
