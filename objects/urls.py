769d6af07db4532a6d4b17fc28b6d84c207b9449
Jordi Armadans <jordiarmadans@MacBook-Pro-de-Jordi.local>
2021-06-24 11:49:59 +0200

    Full MVP Version

--- objects/urls.py
+++ objects/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('lila', views.lila, name='lila'),
+    path('bronce', views.bronce, name='bronce'),
+    path('plata', views.plata, name='plata'),
+    path('oro', views.oro, name='oro'),
     path('thank_you', views.thank_you, name='thank_you'),
 ]
--- objects/views.py
+++ objects/views.py
@@ -7,5 +7,17 @@ from django.contrib import messages
 def home(request):
     return render(request, 'objects/home.html', {})
 
+def bronce(request):
+    return render(request, 'objects/bronce.html', {})
+
+def plata(request):
+    return render(request, 'objects/plata.html', {})
+
+def oro(request):
+    return render(request, 'objects/oro.html', {})
+
+def lila(request):
+    return render(request, 'objects/lila.html', {})    
+
 def thank_you(request):
     return render(request, 'objects/thank_you.html', {})
